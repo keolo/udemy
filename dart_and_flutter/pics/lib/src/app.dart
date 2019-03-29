1ede6deaedd239a74c9f91f92701793da122acd4
Keolo Keagy <keolo@kea.gy>
2019-03-29 12:45:32 -0700

    Added pics project.

---
+++ dart_and_flutter/pics/lib/src/app.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
+import 'widgets/image_list.dart';
+
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    final response =
+        await get('http://jsonplaceholder.typicode.com/photos/$counter');
+    final imageModel = ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Picz'),
+        ),
+        body: ImageList(images),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: fetchImage,
+        ),
+      ),
+    );
+  }
+}
